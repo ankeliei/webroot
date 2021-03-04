95e4927ff60df4ee96c8d8825a0978d2a8292467
ankeliei <2533156094@qq.com>
2021-03-04 22:07:12 +0800

    完成功能， 返回历史总用户量

--- api/userCount.php
+++ api/userCount.php
@@ -1,2 +1,13 @@
 <?php
-echo "2";
+
+require_once "../php/Dbcon.php";
+$con = new Dbcon();
+
+$id = $_REQUEST['id'];
+$sql = "select count(*) as count from users";
+
+$con->setSql($sql);
+$res = $con->getRes();
+$row = $res->fetch_assoc();
+
+echo $row['count'];
