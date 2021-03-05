be73350e6f221e21698299b6f1a2bc322fcea213
ankeliei <2533156094@qq.com>
2021-03-05 16:58:09 +0800

    接收心跳向数据库users表更新数据

--- api/onHeartbeat.php
+++ api/onHeartbeat.php
@@ -1,11 +1,16 @@
 <?php
-//$sessionid = $_COOKIE['JSESSIONID'];
-//session_id($sessionid);
-//session_start();    //恢复session
-//
-//$openid = $_SESSION['openid'];
-//
-//require_once "/php/Dbcon.php";
-//$con = new Dbcon();
+$sessionid = $_COOKIE['JSESSIONID'];
+session_id($sessionid);
+session_start();    //恢复session
+$openid = $_SESSION['openid'];
+$ip = $_SESSION['ip'];
+
+require_once "../php/Dbcon.php";
+$con = new Dbcon();
+
+$sql = "update users set minutes = minutes+1 where (openid = ?)";
+$stmt = $con->prepare($sql);
+$stmt->bind_param("s", $openid);
+$stmt->execute();
 
 echo 0;
